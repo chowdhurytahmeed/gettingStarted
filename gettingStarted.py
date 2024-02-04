8974e9b34f6498402751668d2f24d79cbcc72275
Tahmeed Chowdhury <31861714+chowdhurytahmeed@users.noreply.github.com>
2024-02-03 22:26:51 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -12,9 +12,9 @@ def welcome_assignment_answers(question):
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
-    elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
